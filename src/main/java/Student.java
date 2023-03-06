2b372fecd379448c4734bfc3f1c1b4f5f7486ad2
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-03-06 10:46:46 +0100

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,6 @@
+public class Student {
+    String name;
+    int matrikelnummer;
+    int semestreCount;
+
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+
+        Student newStudent = new Student();
+
+        newStudent.name = "Max";
+        newStudent.matrikelnummer = 44444444;
+        newStudent.semestreCount = 4;
+
+        System.out.println(newStudent.name);
+        System.out.println(newStudent.semestreCount);
+        System.out.println(newStudent.matrikelnummer);
+
+
+    }
+}
